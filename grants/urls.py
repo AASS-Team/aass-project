d3163715778aa1a81348fd2c8889b666f3c27a7b
František Gič <mcfreddie777@gmail.com>
2022-04-14 14:45:40 +0200

    Remove redundant code

--- grants/urls.py
+++ grants/urls.py
@@ -1,10 +1,8 @@
 from django.urls import include, path
-from django.views.generic import RedirectView
 
 from . import views
 
 urlpatterns = [
-    # path("", RedirectView.as_view(url="samples", permanent=False)),
     path(
         "grants/",
         include(
